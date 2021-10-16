34df51e5a2f01b6ea9a40249c72d7f5980707f7a
Trey Gleason <s209058@student.aie.edu>
2021-10-15 17:51:59 -0700

    working on particle system

---
+++ ComputerGraphicsUPT/Assets/Scripts/paticles/ParticleControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    [Header("Emission")]
+    public float rateOverTime = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

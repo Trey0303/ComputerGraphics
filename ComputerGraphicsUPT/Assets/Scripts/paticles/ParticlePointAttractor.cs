ebab865f4f5d8a57c926c21ad65979213415af0b
Trey Gleason <s209058@student.aie.edu>
2021-10-20 17:52:09 -0700

    working on particle system. Particle Point Attractor

---
+++ ComputerGraphicsUPT/Assets/Scripts/paticles/ParticlePointAttractor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePointAttractor : MonoBehaviour
+{
+    public ParticleSystem[] particleSystems;
+
+    public float strength = 1;
+    public float radius = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //int length = particleSystems.GetParticles()
+
+        //for(int particle = 0; particle < particleSystems.Length; particle++)
+        //{
+        //    particleSystems[particle].GetParticles();
+
+        //}
+    }
+}

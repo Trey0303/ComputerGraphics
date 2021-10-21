ef942324b399c251f5c4c49c797d6e3b4beec16a
Trey Gleason <s209058@student.aie.edu>
2021-10-21 13:01:03 -0700

    finished particle systems. working on procedural generation

---
+++ ComputerGraphicsUPT/Assets/Scripts/Procedural Generation/PipeProceduralGeneration.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeProceduralGeneration : MonoBehaviour
+{
+    public int numSegments;
+    public int numFaces;
+    public float radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ////Double Loop: used to create the positions and normals
+        DoubleLoop();
+        
+        //Vector3[] verts = new Vector3[numSegments * numFaces];
+        //Vector3[] normal = new Vector3[numSegments * numFaces];
+        ////get the increment per face around the tube in radians
+        //float deltaPhi = Mathf.PI * 2.0f / numFaces;
+        //DoubleLoop();
+
+        ////UV coordinates and index buffer
+        
+        //Vector2[] uvs = new Vector2[numSegments * numFaces];
+        //int k = 0;
+        //UVDoubleLoop();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DoubleLoop()
+    {
+        Vector3[] verts = new Vector3[numSegments * numFaces]; 
+        Vector3[] normal = new Vector3[numSegments * numFaces];// get the increment per face around the tube in radians
+        float deltaPhi = Mathf.PI * 2.0f / numFaces;
+        for(int i = 0; i < numSegments; i++){
+            for(int j = 0; j < numFaces; j++){
+                int index = i * numFaces + j;
+                float phi = j * deltaPhi;
+                normal[index] = Vector3.right * Mathf.Cos(phi) + Vector3.forward * Mathf.Sin(phi);
+                Vector3 centre = i * Vector3.up;
+                verts[index] = centre + normal[index] * radius;
+            }
+        }
+    }
+
+    void UVDoubleLoop()
+    {
+        for(int i = 0; i < numSegments; i++)
+        {
+            for(int j = 0; j < numFaces; j++)
+            {
+
+            }
+        }
+    }
+}
--- ComputerGraphicsUPT/Assets/Scripts/paticles/ParticlePointAttractor.cs
+++ ComputerGraphicsUPT/Assets/Scripts/paticles/ParticlePointAttractor.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//set the particale particles systems to 'world' simulation space to get it to move towards the direction you want the particales to move to
 public class ParticlePointAttractor : MonoBehaviour
 {
-    public ParticleSystem[] particleSystems;
+    public ParticleSystem parSystem;
+    public ParticleSystem.Particle[] particles;
+    public Transform target;
+
+    
 
     public float strength = 1;
     public float radius = 1;
@@ -12,18 +18,43 @@ public class ParticlePointAttractor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        particles = new ParticleSystem.Particle[parSystem.maxParticles];
     }
 
     // Update is called once per frame
     void Update()
     {
-        //int length = particleSystems.GetParticles()
 
-        //for(int particle = 0; particle < particleSystems.Length; particle++)
-        //{
-        //    particleSystems[particle].GetParticles();
+        float distance = Vector3.Distance(target.position, parSystem.transform.position);
+        //if in radius
+        if (distance < radius)
+        {
+            int numParticles = parSystem.GetParticles(particles);
+            for (int i = 0; i < numParticles; i++)
+            {
+                //Debug.Log(particles[i].position);
+                //Debug.Log("Particle: " + i);
 
-        //}
+                distance = Vector3.Distance(target.position, parSystem.transform.position);
+
+                //Debug.Log("in range");
+                //move particles towards target
+                if (distance > 0.1f)
+                {
+                    Debug.DrawLine(parSystem.transform.position, target.position, Color.white);
+                    //Debug.Log("Move");
+                    particles[i].position = Vector3.MoveTowards(particles[i].position, target.position, strength * Time.deltaTime);
+
+                }
+
+            }
+            parSystem.SetParticles(particles, numParticles);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, new Vector3(radius, radius, radius));
     }
 }

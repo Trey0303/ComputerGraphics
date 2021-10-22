eeaedcaf89bd5aaec409586c458d8e53cf00b850
Trey Gleason <s209058@student.aie.edu>
2021-10-21 17:51:21 -0700

    working on postProssessing volume triggers. worked a bit on ui and items

---
+++ ComputerGraphicsUPT/Assets/Scripts/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if(player.gameObject.tag == "Player")
+        {
+            //Debug.Log("picked up item");
+            if (gameObject.tag == "Orange")
+            {
+                Debug.Log("Orange");
+                Destroy(gameObject);
+            }
+            if (gameObject.tag == "Green")
+            {
+                Debug.Log("Green");
+                Destroy(gameObject);
+            }
+            if (gameObject.tag == "Purple")
+            {
+                Debug.Log("Purple");
+                Destroy(gameObject);
+            }
+        }
+        
+    }
+}

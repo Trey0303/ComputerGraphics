640c335535aab63ca69008e82f084c41c95c591d
Trey Gleason <s209058@student.aie.edu>
2021-10-27 17:48:57 -0700

    finished unity shader graph exercises. Currently working on unity shader graph(advanced)

---
+++ ComputerGraphicsUPT/Assets/materialVisualDebug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class materialVisualDebug : MonoBehaviour
+{
+
+    public float radius = 1.0f;
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}

0dd4f230c0eac4b85f2a1f4e4a12e929c30f4ede
Trey Gleason <s209058@student.aie.edu>
2021-11-18 10:56:42 -0800

    added a damage collision script for spikes

---
+++ ComputerGraphicsUPT/Assets/Scripts/DamageCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageCollision : MonoBehaviour
+{
+    public Health health;
+
+
+//note: have to enable contact pairs for kinematic to static in project settings
+//      (edit > project settings > physics > contact pairs mode)
+    private void OnCollisionEnter(Collision other)
+    {
+            Debug.Log("touched spikes");
+        if (other.gameObject.tag == "Player")
+        {
+
+            //Debug.Log("player takes damage");
+            health.TakeDamage(1);
+
+        }
+    }
+}
